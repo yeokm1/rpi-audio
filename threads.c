0814da0e2c9ef4438b5e73731af417d249e13fee
Yeo Kheng Meng <yeokm1@yahoo.com.sg>
2015-01-25 01:28:59 +0800

    Update threads.c
    untested

--- threads.c
+++ threads.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
 #include <pthread.h>
-#define BUFFERSIZE 10
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
 
+#define BUFFERSIZE 10 
 int percent = 0;
+
+int open_port(void)
+ {
+   int fd; /* File descriptor for the port */
+
+
+   fd = open("/dev/ttym1", O_RDWR | O_NOCTTY | O_NDELAY);
+   if (fd == -1)
+   {
+    /*
+     * Could not open the port.
+     */
+
+     fprintf(stderr, "open_port: Unable to open /dev/ttym1 - %s\n",
+             strerror(errno));
+   }
+
+   return (fd);
+ }
+
+
+
 void *thread_function( void *ptr ){
 	
-	char buffer[BUFFERSIZE];
+	char buffer;
+	char writeBuffer[5];
 	int result;
 	while(1){
-		sleep(1);
-		if(fgets(buffer, BUFFERSIZE , stdin) != NULL){
-			percent = strtol(buffer, (char **)NULL, 10);
+		//sleep(1);
+		//fcntl(fd, F_SETFL, FNDELAY); 
+		read(fd, &buffer, sizeof(buffer));
+		if(buffer == 'u'){
+			percent++;
+		} else if(buffer == 'd'){
+			percent--;
 		}
 		
+		itoa(percent, writeBuffer, 10);
+		write(fd, writeBuffer, 4);
 	}
 }
 
 int main()
 {
+     fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
      pthread_t thread;
 
      pthread_create( &thread, NULL, thread_function, "thread");
